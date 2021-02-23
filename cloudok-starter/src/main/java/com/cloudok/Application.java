d524a30d2ff462953ec61329a8c26ef16e0fecc6
tiebin.zhang <tiebin.zhang@gmail.com>
2021-02-23 09:19:04 +0800

    文件存储

--- cloudok-starter/src/main/java/com/cloudok/Application.java
+++ cloudok-starter/src/main/java/com/cloudok/Application.java
@@ -2,13 +2,19 @@ package com.cloudok;
 
 import java.util.Properties;
 
+import javax.servlet.MultipartConfigElement;
+
 import org.apache.ibatis.mapping.DatabaseIdProvider;
 import org.apache.ibatis.mapping.VendorDatabaseIdProvider;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.util.unit.DataSize;
+import org.springframework.util.unit.DataUnit;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -21,6 +27,9 @@ import com.cloudok.base.dict.enums.scan.EnumSan;
 @EnumSan(basePackages = "com.cloudok")
 public class Application {
 
+	@Value("${server.tomcat.basedir:/tmp/api}")
+	private String temp;
+	
 	private CorsConfiguration buildConfig() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.addAllowedHeader("*");
@@ -45,6 +54,17 @@ public class Application {
 		databaseIdProvider.setProperties(p);
 		return databaseIdProvider;
 	}
+	
+	@Bean
+	public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setLocation(temp);
+        factory.setMaxFileSize(DataSize.of(50, DataUnit.MEGABYTES));
+        factory.setMaxRequestSize(DataSize.of(50, DataUnit.MEGABYTES));
+        factory.setFileSizeThreshold(DataSize.of(100, DataUnit.MEGABYTES));
+        return factory.createMultipartConfig();
+
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);

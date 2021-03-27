9b1b13664ea0ae53bda08a916d83e5e121ddd76a
tiebin.zhang <tiebin.zhang@gmail.com>
2021-03-27 17:13:22 +0800

    update sqls

--- cloudok-starter/src/main/resources/db/migration/mysql/V20210325100500__update_tags.sql
+++ cloudok-starter/src/main/resources/db/migration/mysql/V20210325100500__update_tags.sql
@@ -1,3 +1,2 @@
 UPDATE `base_tag` SET `relation_to` = '4387233486537228362' WHERE (`id` = '16');
-UPDATE `base_tag` SET `relation_to` = '4387233486537228349' WHERE (`id` = '10');
 UPDATE `base_tag` SET `relation_to` = '4387233486537228365' WHERE (`id` = '15');
